ffea218e43547d35b43db4a41c3e64562b73a057
Pedro Poliszuk <124004844+pedropoliszuk@users.noreply.github.com>
2023-03-12 14:29:29 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,28 @@
+const botoesCarrossel = document.querySelectorAll('.botao');
+const imagens = document.querySelectorAll('.imagem');
+botoesCarrossel.forEach((botao, indice) => {
+    botao.addEventListener('click', () => {
+        desativarBotaoSelecionado();
+        selecionarBotaoCarrossel(botao);
+        esconderImagemAtiva();
+        mostrarImagemFundo(indice);
+    })
+})
+
+function mostrarImagemFundo(indice) {
+    imagens[indice].classList.add('ativa');
+}
+
+function selecionarBotaoCarrossel(botao) {
+    botao.classList.add('selecionado');
+}
+
+function esconderImagemAtiva() {
+    const imagemAtiva = document.querySelector('.ativa');
+    imagemAtiva.classList.remove('ativa');
+}
+
+function desativarBotaoSelecionado() {
+    const botaoSelecionado = document.querySelector('.selecionado');
+    botaoSelecionado.classList.remove('selecionado');
+}
